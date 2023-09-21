e99969b598ae01a5ce8da010b6588f214853d15b
infgsagarna <infgsagarna@elorrieta-errekamari.com>
2023-09-21 11:13:39 +0200

    cambio2

--- app/src/main/java/com/example/aplicacionprueba/MainActivity.java
+++ app/src/main/java/com/example/aplicacionprueba/MainActivity.java
@@ -10,10 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setup();
+
     }
 
-    private void setup() {
-    }
 
 }
